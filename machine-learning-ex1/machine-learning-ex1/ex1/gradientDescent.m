5ea13bb720f36cfe0fd58cbd7ac4a27b8e83b3c8
codingkohli <aditya.12.kohli@gmail.com>
2018-01-11 19:20:21 -0500

    Completing Linear Regression

--- machine-learning-ex1/machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,13 +16,24 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+	%temp1 represents derivative for x0
+	temp1 = 0;
+	%temp2 represents derivative for x1
+	temp2 = 0;
+	for i = 1:m  
+		%tempH represents h(theta)
+		tempH = theta(1) + theta(2)*X(i,2);
+		tempH = tempH - y(i) ;
+		temp1 = temp1 + tempH;
+		temp2 = temp2 + tempH * X(i,2);
+	end
+	temp1 = temp1 * (alpha/m) ;
+	temp2 = temp2 * (alpha/m) ;
 	
+	%updating the values of theta	
+	 theta(1) = theta(1) - temp1;
+	 theta(2) = theta(2) - temp2;
 	
-
-
-
-
-
     % ============================================================
 
     % Save the cost J in every iteration    
